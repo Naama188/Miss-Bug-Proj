1f9f05592b585f89555786c205f5097547337cc9
Naama Tanne <naama.t@gmail.com>
2025-03-24 13:56:28 +0200

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/', (req, res) => res.send('Hello there'))
+
+app.listen(3030, () => console.log('Server ready at port 3030'))
